60b846a5235d470b890f6bae3359f63ef45387ae
linhcon24 <nguyenkhanhlinhh24@gmail.com>
2023-09-30 23:02:39 +0700

    test

--- src/main/java/com/example/duantotnghiepgiaythethaonova/Model/SanPham.java
+++
@@ -1,4 +0,0 @@
-package com.example.duantotnghiepgiaythethaonova.Model;
-
-public class SanPham {
-}
---
+++ src/main/java/com/example/duantotnghiepgiaythethaonova/entity/SanPham.java
@@ -0,0 +1,4 @@
+package com.example.duantotnghiepgiaythethaonova.entity;
+
+public class SanPham {
+}
