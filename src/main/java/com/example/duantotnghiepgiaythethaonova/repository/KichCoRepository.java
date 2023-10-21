7b2f59bf5e335ddc7e68a44c4b01339650ea6a8d
linhcon24 <nguyenkhanhlinhh24@gmail.com>
2023-10-22 00:02:31 +0700

    Update @Repository

--- src/main/java/com/example/duantotnghiepgiaythethaonova/repository/KichCoRepository.java
+++ src/main/java/com/example/duantotnghiepgiaythethaonova/repository/KichCoRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface KichCoRepository extends JpaRepository<KichCo,Integer> {
     @Query(value = "SELECT * FROM KichCo c WHERE c.DaXoa = 0 ORDER BY c.IdKichCo DESC", nativeQuery = true)
     List<KichCo> selectAllKichCoExist();
--- src/main/java/com/example/duantotnghiepgiaythethaonova/repository/MauSacRepository.java
+++ src/main/java/com/example/duantotnghiepgiaythethaonova/repository/MauSacRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface MauSacRepository extends JpaRepository<MauSac,Integer> {
 
     @Query(value = "SELECT * FROM MauSac c WHERE c.DaXoa = 0 ORDER BY c.IdMauSac DESC", nativeQuery = true)
