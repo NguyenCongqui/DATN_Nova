af1487a0fcd027f022b48b6a64ad68719cfc2652
DESKTOP-VMKQ8H2\ADMIN <quincph23871@fpt.edu.vn>
2023-10-23 13:15:45 +0700

    quy

--- src/main/java/com/example/duantotnghiepgiaythethaonova/QEntityGenarate/QBaseEntity.java
+++ src/main/java/com/example/duantotnghiepgiaythethaonova/QEntityGenarate/QBaseEntity.java
@@ -1,15 +1,13 @@
 package com.example.duantotnghiepgiaythethaonova.QEntityGenarate;
 
-import com.example.duantotnghiepgiaythethaonova.entity.BaseEntity;
-import com.querydsl.core.annotations.Generated;
-import com.querydsl.core.types.Path;
-import com.querydsl.core.types.PathMetadata;
-import com.querydsl.core.types.dsl.DateTimePath;
-import com.querydsl.core.types.dsl.EntityPathBase;
-import com.querydsl.core.types.dsl.NumberPath;
-import com.querydsl.core.types.dsl.StringPath;
+import static com.querydsl.core.types.PathMetadataFactory.*;
 
-import static com.querydsl.core.types.PathMetadataFactory.forVariable;
+import com.example.duantotnghiepgiaythethaonova.entity.BaseEntity;
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
 
 
 /**
@@ -18,11 +16,9 @@ import static com.querydsl.core.types.PathMetadataFactory.forVariable;
 @Generated("com.querydsl.codegen.DefaultSupertypeSerializer")
 public class QBaseEntity extends EntityPathBase<BaseEntity> {
 
-    private static final Integer serialVersionUID = 1768552950;
+    private static final long serialVersionUID = -1172130713L;
 
     public static final QBaseEntity baseEntity = new QBaseEntity("baseEntity");
-
-    public final NumberPath<Long> id = createNumber("id", Long.class);
 
     public final DateTimePath<java.util.Date> ngayCapNhat = createDateTime("ngayCapNhat", java.util.Date.class);
 
