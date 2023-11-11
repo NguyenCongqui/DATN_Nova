91e600c2bd97fe219e57777421fa23b333a816de
linhcon24 <nguyenkhanhlinhh24@gmail.com>
2023-11-11 18:39:48 +0700

    Update Search HoaDonTaiQuay

--- src/main/java/com/example/duantotnghiepgiaythethaonova/controller/admin/HoaDon/DonTaiQuay/DaHuyTaiQuayController.java
+++ src/main/java/com/example/duantotnghiepgiaythethaonova/controller/admin/HoaDon/DonTaiQuay/DaHuyTaiQuayController.java
@@ -4,6 +4,7 @@ import com.example.duantotnghiepgiaythethaonova.service.DonHangTaiQuayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +18,23 @@ public class DaHuyTaiQuayController {
                                @RequestParam(defaultValue = "1") int page,
                                @RequestParam(defaultValue = "5") int size) {
         donHangTaiQuayService.daHuyTaiQuay(model, page, size);
+        return "admin/hoadon/DonTaiQuay/daHuyTaiQuay";
+    }
+    @RequestMapping("admin/DaHuyTaiQuay/TimKiem/{duLieuTimKiem}")
+    public String timKiemHoaDOnTaiQuayDaHuy(Model model,
+                                            @RequestParam(defaultValue = "1") int page,
+                                            @RequestParam(defaultValue = "5") int size,
+                                            @PathVariable("duLieuTimKiem") String duLieuTimKiem) {
+        donHangTaiQuayService.timKiemHoaDOnTaiQuayDaHuy(model, page, size, duLieuTimKiem);
+        return "admin/hoadon/DonTaiQuay/daHuyTaiQuay";
+    }
+
+    @RequestMapping("admin/DaHuyTaiQuay/Ngay/{duLieuTimKiem}")
+    public String timKiemTheoNgayHoaDontaiQuayDaHuy(Model model,
+                                                    @RequestParam(defaultValue = "1") int page,
+                                                    @RequestParam(defaultValue = "5") int size,
+                                                    @PathVariable("duLieuTimKiem") String duLieuTimKiemString) {
+        donHangTaiQuayService.timKiemTheoNgayHoaDontaiQuayDaHuy(model, page, size, duLieuTimKiemString);
         return "admin/hoadon/DonTaiQuay/daHuyTaiQuay";
     }
 }
--- src/main/java/com/example/duantotnghiepgiaythethaonova/controller/admin/HoaDon/DonTaiQuay/DaThanhToanController.java
+++ src/main/java/com/example/duantotnghiepgiaythethaonova/controller/admin/HoaDon/DonTaiQuay/DaThanhToanController.java
@@ -4,6 +4,7 @@ import com.example.duantotnghiepgiaythethaonova.service.DonHangTaiQuayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,5 +21,24 @@ public class DaThanhToanController {
         donHangTaiQuayService.DanhSachDaThanhToan(model, page, size);
         return "admin/hoadon/DonTaiQuay/daThanhToan";
     }
+
+    @RequestMapping("admin/DaThanhToan/timKiem/{duLieuTimKiem}")
+    public String timKiemHoaDonDaThanhToan(Model model,
+                                           @RequestParam(defaultValue = "1") int page,
+                                           @RequestParam(defaultValue = "5") int size,
+                                           @PathVariable("duLieuTimKiem") String duLieuTimKiem) {
+        donHangTaiQuayService.timKiemHoaDonDaThanhToan(model, page, size, duLieuTimKiem);
+        return "admin/hoadon/DonTaiQuay/daThanhToan";
+    }
+
+    @RequestMapping("admin/DaThanhToan/Ngay/{duLieuTimKiem}")
+    public String timKiemHoaDonDaThanhToanTheoNgayTao(Model model,
+                                                      @RequestParam(defaultValue = "1") int page,
+                                                      @RequestParam(defaultValue = "5") int size,
+                                                      @PathVariable("duLieuTimKiem") String duLieuTimKiemString) {
+        donHangTaiQuayService.timKiemHoaDonDaThanhToanTheoNgayTao(model, page, size, duLieuTimKiemString);
+        return "admin/hoadon/DonTaiQuay/daThanhToan";
+    }
+
 
 }
